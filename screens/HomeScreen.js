c15e0380893d6cb85d96368ef40d4671d43a7132
Deepak <deepsrh1801@gmail.com>
2021-10-30 19:37:04 +0530

    done with stories components

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,12 +2,14 @@ import React from "react";
 import { View, Text, SafeAreaView, StyleSheet } from "react-native";
 import GlobalStyles from "../GlobalStyles";
 import Header from "../components/Header";
+import Stories from "../components/Stories";
 
 function HomeScreen() {
   return (
     <SafeAreaView style={styles.container}>
       <SafeAreaView style={GlobalStyles.droidSafeArea}>
         <Header />
+        <Stories />
       </SafeAreaView>
     </SafeAreaView>
   );
