77fbb7ac9df8612ac112b004532f969a186934a3
Deepak <deepsrh1801@gmail.com>
2021-12-18 22:04:18 +0530

    added signupform

---
+++ screens/SignupScreen.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { View, Text, Image, StyleSheet, SafeAreaView } from "react-native";
+import GlobalStyles from "../GlobalStyles";
+import SignupForm from "../components/SignupScreen/SignupForm";
+
+const INSTAGRAM_LOGO =
+  "https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Instagram_colored_svg_1-512.png";
+
+const SignupScreen = () => {
+  return (
+    <View style={GlobalStyles.droidSafeArea}>
+      <View style={styles.container}>
+        <View style={styles.logoContainer}>
+          <Image source={{ uri: INSTAGRAM_LOGO, height: 100, width: 100 }} />
+        </View>
+      <SignupForm />
+      </View>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  logoContainer: {
+    alignItems: "center",
+    marginTop: 60,
+  },
+});
+
+export default SignupScreen;
